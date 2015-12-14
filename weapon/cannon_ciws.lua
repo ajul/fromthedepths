970baf38e7782d24aed159be6e5fea34e12db225
Albert Julius Liu <ajul1987@gmail.com>
2015-12-13 23:55:04 -0800

    allow fire at vehicles if no missile in range

--- weapon/cannon_ciws.lua
+++ weapon/cannon_ciws.lua
@@ -1,15 +1,14 @@
 -- All turrets in this weapon slot are assumed to be CIWS.
 ciwsWeaponSlot = 5
 
--- Weapon speed for computing spinner lead.
--- nil to autodetect.
-spinnerWeaponSpeed = nil
-
 -- What offset to consider firing weapon, where 0 is (hopefully) a direct hit.
 -- Min offset should be negative and slightly smaller than the explosive radius.
 -- Max offset should be zero or a small number.
 minFireOffset = -8.0
 maxFireOffset = 2.0
+
+-- If greater than 0, we will attack vehicles if no missile can be fired at.
+vehicleFireOffset = 15.0
 
 -- Length of the barrel.
 barrelLength = 8.0
@@ -33,6 +32,12 @@ I = nil
 -- Table of known enemy missiles.
 warnings = {}
 
+-- Target vehicle.
+targets = {}
+
+-- Weapon speed for computing spinner lead.
+spinnerWeaponSpeed = 200
+
 function UpdateWarnings()
     -- Called first. This updates the warning table. Elements are warningIndex1 -> warningInfo.
     warnings = {}
@@ -50,6 +55,23 @@ function UpdateWarnings()
     end
 end
 
+function UpdateTargets()
+    targets = {}
+    for mainframeIndex = 0, I:GetNumberOfMainframes() - 1 do
+        local potentialTarget = I:GetTargetInfo(mainframeIndex, 0)
+        if potentialTarget.Valid then
+            for targetIndex0 = 0, I:GetNumberOfTargets(mainframeIndex) - 1 do
+                local target = I:GetTargetInfo(mainframeIndex, targetIndex0)
+                targets[targetIndex0 + 1] = {
+                    Position = target.AimPointPosition,
+                    Velocity = target.Velocity,
+                }
+            end
+            break
+        end
+    end
+end
+
 function QuaternionRightVector(quaternion)
     local x = 1 - 2 * (quaternion.y * quaternion.y - quaternion.z * quaternion.z) 
     local y = 2 * (quaternion.x * quaternion.y + quaternion.z * quaternion.w )
@@ -60,6 +82,7 @@ end
 function Update(Iarg)
     I = Iarg
     UpdateWarnings()
+    UpdateTargets()
     for turretSpinnerIndex = 0, I:GetTurretSpinnerCount() - 1 do
         for weaponIndex = 0, I:GetWeaponCountOnTurretOrSpinner(turretSpinnerIndex) do
             local weaponInfo = I:GetWeaponInfoOnTurretOrSpinner(turretSpinnerIndex, weaponIndex)
@@ -78,7 +101,7 @@ end
 function AimSpinner(spinnerIndex)
     local spinner = I:GetSpinnerInfo(spinnerIndex)
     -- Find a target.
-    local selectedWarning, _ = SelectWarning(spinner.Position, spinnerWeaponSpeed or 200)
+    local selectedWarning, _ = SelectWarning(spinner.Position, spinnerWeaponSpeed)
     if selectedWarning ~= nil then
         local relativeTargetPosition = selectedWarning.Position - spinner.Position + selectedWarning.Velocity * fuseTime + gravityDrop
         -- rotate spinner
@@ -98,9 +121,7 @@ end
 function AimSpinnerWeapon(turretSpinnerIndex, weaponIndex)
     local weapon = I:GetWeaponInfoOnTurretOrSpinner(turretSpinnerIndex, weaponIndex)
     local selectedWarning, shouldFire = SelectWarning(weapon.GlobalPosition, weapon.Speed)
-    if spinnerWeaponSpeed == nil then
-        spinnerWeaponSpeed = weapon.Speed
-    end
+    spinnerWeaponSpeed = weapon.Speed
     if selectedWarning ~= nil then
         local relativeTargetPosition = selectedWarning.Position - weapon.GlobalPosition + selectedWarning.Velocity * fuseTime + gravityDrop
         
@@ -113,26 +134,40 @@ function AimSpinnerWeapon(turretSpinnerIndex, weaponIndex)
     end
 end
 
+function ComputeCloseRateAndOffset(warning, position, weaponSpeed)
+    local relativePosition = warning.Position - position
+    local closeRate = -Vector3.Dot(warning.Velocity, relativePosition.normalized)
+    -- Adjust position for gravity.
+    local adjustedRelativePosition = relativePosition + gravityDrop
+    -- How far warning will be after fuseTime, relative to center of intercept window.
+    local interceptOffset = adjustedRelativePosition.magnitude - fuseTime * (weaponSpeed + closeRate) - barrelLength
+    return closeRate, interceptOffset
+end
+
 function SelectWarning(position, weaponSpeed)
     local selectedWarning = nil
-    local selectedRadius = 10000
+    local selectedOffset = 10000
     local shouldFire = false
     for warningIndex, warning in ipairs(warnings) do
-        local relativePosition = warning.Position - position
-        local closeRate = -Vector3.Dot(warning.Velocity, relativePosition.normalized)
-        -- Adjust position for gravity.
-        local adjustedRelativePosition = relativePosition + gravityDrop
-        -- How far warning will be after fuseTime, relative to center of intercept window.
-        local interceptOffset = adjustedRelativePosition.magnitude - fuseTime * (weaponSpeed + closeRate) - barrelLength
-        -- I:Log(string.format("Close in %0.1f seconds, intercept radius %0.1f", closeTime, interceptOffset))
-        -- Pick closest missile that is not too late to intercept.
-        -- I:LogToHud(string.format("%d: %0.1f", warningIndex, interceptOffset))
-        if closeRate > 0 and interceptOffset < selectedRadius and interceptOffset > minFireOffset then
+        closeRate, interceptOffset = ComputeCloseRateAndOffset(warning, position, weaponSpeed)
+        -- I:LogToHud(string.format("Warning %d: %0.1f", warningIndex, interceptOffset))
+        if interceptOffset < selectedOffset and interceptOffset > minFireOffset then
             if interceptOffset < maxFireOffset then
                 shouldFire = true
             end
             selectedWarning = warning
-            selectedRadius = interceptOffset
+            selectedOffset = interceptOffset
+        end
+    end
+    if not shouldFire and vehicleFireOffset > 0.0 then
+        for targetIndex, target in ipairs(targets) do
+            _, interceptOffset = ComputeCloseRateAndOffset(target, position, weaponSpeed)
+            -- I:LogToHud(string.format("Target %d: %0.1f", targetIndex, interceptOffset))
+            if math.abs(interceptOffset) < vehicleFireOffset then
+                shouldFire = true
+                selectedWarning = target
+                break
+            end
         end
     end
     return selectedWarning, shouldFire
