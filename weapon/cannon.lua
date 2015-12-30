bc6136eab1b74a8104f1e110009f900d1487a241
Albert Julius Liu <ajul1987@gmail.com>
2015-12-29 22:19:20 -0800

    fix acceleration computation, but may need to move to direct position differencing

--- weapon/cannon.lua
+++ weapon/cannon.lua
@@ -1,8 +1,8 @@
 -- Weapon slot to use. Only cannons will be controlled regardless.
 weaponSlot = 1
 
--- Length of the barrel.
-barrelLength = 8.0
+-- Extra time to lead the target by.
+extraLeadTime = 1/40
 
 -- Will attempt to aim this proportion of the way towards the target each frame.
 -- Should be below 1.
@@ -11,15 +11,20 @@ spinGain = 0.9
 
 leadIterations = 16
 
-frameTime = 1/40
 g = 9.81
 
 -- The I in Update(I).
 I = nil
 
+nominalFrameDuration = 1/40
+
+-- Last frame for which there was acceleration.
+lastAccelerationTime = nil
+
+DEFAULT_ACCELERATION_DURATION = 0.5
+
 -- Current target.
 target = nil
-
 targetAcceleration = Vector3.zero
 relativeVelocity = Vector3.zero
 
@@ -31,7 +36,7 @@ WEAPON_TYPE_CANNON = 0
 function Update(Iarg)
     I = Iarg
     
-    UpdateTarget()
+    UpdateInfo()
 
     if target ~= nil then
         for turretSpinnerIndex = 0, I:GetTurretSpinnerCount() - 1 do
@@ -58,7 +63,7 @@ function Update(Iarg)
     end
 end
 
-function UpdateTarget()
+function UpdateInfo()
     local newTarget = nil
     for mainframeIndex = 0, I:GetNumberOfMainframes() - 1 do
         local firstTarget = I:GetTargetInfo(mainframeIndex, 0)
@@ -71,9 +76,23 @@ function UpdateTarget()
     
     -- Update acceleration if target is same as previous update.
     if newTarget ~= nil and target ~= nil and newTarget.Id == target.Id then
-        targetAcceleration = newTarget.Velocity - target.Velocity
+        local velocityChange = newTarget.Velocity - target.Velocity
+        if velocityChange.magnitude > 0 then
+            local currentTime = I:GetGameTime()
+            local accelerationDuration
+            if lastAccelerationTime ~= nil then 
+                accelerationDuration = currentTime - lastAccelerationTime
+            else
+                accelerationDuration = DEFAULT_ACCELERATION_DURATION
+            end
+            lastAccelerationTime = currentTime
+            targetAcceleration = velocityChange / accelerationDuration
+            
+            I:LogToHud(string.format("Acceleration: %f (for %0.3f s)", targetAcceleration.magnitude, accelerationDuration))
+        end
         relativeVelocity = target.Velocity - I:GetVelocityVector()
     else
+        lastAccelerationTime = nil
         targetAcceleration = Vector3.zero
         relativeVelocity = Vector3.zero
     end
@@ -92,7 +111,7 @@ function AimSpinner(spinnerIndex)
         local s = Vector3.Dot(aim.normalized, spinnerRight.normalized)
         -- domain is [-pi, pi]
         local targetAngle = math.atan2(s, c)
-        local spinSpeed = targetAngle * spinGain / frameTime
+        local spinSpeed = targetAngle * spinGain / nominalFrameDuration
         I:SetSpinnerContinuousSpeed(spinnerIndex, spinSpeed)
         
         return
@@ -127,7 +146,8 @@ function ComputeAim(weaponPosition, weaponSpeed)
         end
         aim = relativePosition + ComputeLead(t)
     end
-    I:LogToHud(string.format("Lead time: %0.2f s", t))
+    --I:LogToHud(string.format("Lead time: %0.3f s", t))
+    --I:LogToHud(string.format("Acceleration: %f, frame time %f", targetAcceleration.magnitude, lastFrameTime))
     return aim, t
 end
 
@@ -146,7 +166,9 @@ function ComputeFlightTime(aim, weaponSpeed)
         local width = math.sqrt(discriminant)
         local lower = vertex - width
         local upper = vertex + width
-        return (lower >= 0 and math.sqrt(lower)) or (upper >= 0 and math.sqrt(upper)) or nil
+        return (lower >= 0 and math.sqrt(lower) + extraLeadTime) or 
+               (upper >= 0 and math.sqrt(upper) + extraLeadTime) or 
+               nil
     else
         return nil
     end
