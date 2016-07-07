aa1dcb01183a9294ef7ee260d2b7f8343856a081
ajul <ajul1987@gmail.com>
2016-07-06 23:46:03 -0400

    remove log

--- missile/fuse/timed_fuse.lua
+++ missile/fuse/timed_fuse.lua
@@ -5,8 +5,6 @@ function Update(I)
         for m = 0, I:GetLuaControlledMissileCount(t) - 1 do
             missile = I:GetLuaControlledMissileInfo(t, m)
             if missile.TimeSinceLaunch >= fuseTime then
-                I:LogToHud(missile.Velocity.magnitude)
-                I:Log(missile.Velocity.magnitude)
                 I:DetonateLuaControlledMissile(t, m)
             end
         end
