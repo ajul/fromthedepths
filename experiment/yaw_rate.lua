4e8d39518f383c1a1e953be50741dd224c0608c3
ajul <ajul1987@gmail.com>
2016-10-15 12:07:14 -0400

    acceleration, turn radius

--- experiment/yaw_rate.lua
+++ experiment/yaw_rate.lua
@@ -4,12 +4,13 @@ minDisplayYawRate = 0.5 -- don't display anything if yaw rate is under this thre
 I = nil
 prevGameTime = nil
 prevYaw = 0.0
-prevYawRate = 0.0
+meanYawRate = 0.0
 
 function Update(Iarg)
     I = Iarg
     
     local yaw = I:GetConstructYaw()
+    local speed = I:GetVelocityMagnitude()
     local gameTime = I:GetGameTime()
     
     if prevGameTime ~= nil then
@@ -20,9 +21,11 @@ function Update(Iarg)
         end
         local yawRate = yawChange / dt
         local alpha = math.exp(-dt / tau)
-        prevYawRate = alpha * prevYawRate + (1.0 - alpha) * yawRate
-        if prevYawRate >= minDisplayYawRate then
-            LogBoth(string.format('Yaw rate: %0.1f', prevYawRate))
+        meanYawRate = alpha * meanYawRate + (1.0 - alpha) * yawRate
+        if meanYawRate >= minDisplayYawRate then
+            local turnRadius = speed / math.rad(meanYawRate)
+            local turnAcceleration = speed * math.rad(meanYawRate)
+            LogBoth(string.format('Yaw rate: %0.1f, turn radius %d, turn acceleration %0.2f', meanYawRate, turnRadius, turnAcceleration))
         end
     end
     
